f537fa1263c3e092751b0cdb39b370cbeaeca1a6
Minifixio <emile@le-gallic.com>
2016-12-15 14:13:30 +0100

    update widget.py

--- Bleemeo_Dashboard/widget.py
+++ Bleemeo_Dashboard/widget.py
@@ -5,22 +5,11 @@ from random import randint
 cpu = randint(0, 100)
 
 
-"""class NewClientsWidget(NumberWidget):
-    title = 'CPU'
 
-    def get_value(self):
-        global users
-        users = randint(0, 100)
-        return users
-
-    def get_detail(self):
-        global users
-
-    def get_more_info(self):
-        global users"""
 
 class NewClientsWidget(Widget):
-    title = 'CPU2'
+    title = 'CPU'
+    data = []
 
     def get_title(self):
         return self.title
@@ -28,8 +17,52 @@ class NewClientsWidget(Widget):
     def get_cpu(self):
         return randint(0, 100)
 
+    def get_data(self):
+        return self.data
+
     def get_context(self):
         return {
             'title': self.get_title(),
             'cpu': self.get_cpu(),
-        }
+            'data': self.get_data(),
+}
+
+class NewClientsWidget(Widget):
+    title = 'Meteo'
+    data = []
+
+    def get_title(self):
+        return self.title
+
+    def get_data(self):
+        return self.data
+
+    def get_number(self):
+        return randint(50, 90)
+
+    def get_context(self):
+        return {
+            'title': self.get_title(),
+            'data': self.get_data(),
+            'cpu': self.get_number(),
+}
+
+class NewClientsWidget(Widget):
+    title = 'Memory'
+    data = []
+
+    def get_title(self):
+        return self.title
+
+    def get_memory(self):
+        return randint(50, 90)
+
+    def get_data(self):
+        return self.data
+
+    def get_context(self):
+        return {
+            'title': self.get_title(),
+            'momory': self.get_memory(),
+            'data': self.get_data(),
+}
